688fdbd53a7b002f60d0b78e9bdbdc9d6ee40ba6
Layo Folaranmi <thedarkkid.codes@gmail.com>
2020-09-03 17:26:17 +0400

    modified the obstacle object with accessors to its html node and an Out method

--- src/core/Obstacle.ts
+++ src/core/Obstacle.ts
@@ -1,13 +1,28 @@
+import ObstacleFactory from "./ObstacleFactory";
+
 export default class Obstacle{
-    private node_: HTMLDivElement;
-    private bottom: number =  Math.ceil(Math.random() * 60);
+    private readonly node_: HTMLDivElement;
+    private readonly bottom: number =  Math.ceil(Math.random() * 60);
     private left: number = 500;
 
     constructor(_node: HTMLDivElement) {
         this.node_ = _node;
-
         this.node_.style.bottom = this.bottom + 'px';
         this.node_.style.left = this.left + 'px';
         this.node_.classList.add("obstacle");
     }
+
+    slide = (distance: number): void => {
+        this.left -= distance;
+        this.node_.style.left = this.left + 'px';
+    };
+
+    get Node(){
+        return this.node_;
+    };
+
+    get Out(){
+        return this.left <= -60;
+    }
+
 }
